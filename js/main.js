368346f820040804c3f685a8f593586d4942af84
zijadddd <94227360+zijadddd@users.noreply.github.com>
2022-05-07 09:45:17 +0200

    git push --all origin

--- js/main.js
+++ js/main.js
@@ -18,22 +18,7 @@ const deleteNewsMessage = () => {
     div.style.display = "none";
 
 }
-/*
-document.addEventListener("DOMContentLoaded", () => {
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 50) {
-            document.getElementById('navbar_top').classList.add('fixed-top');
-            navbar_height = document.querySelector('.navbar').offsetHeight;
-            document.body.style.paddingTop = navbar_height + 'px';
-            document.getElementById('navbar_top').style.width = '80%';
-        } else {
-            document.getElementById('navbar_top').classList.remove('fixed-top');
-            document.body.style.paddingTop = '0';
-            document.getElementById('navbar_top').style.width = '100%';
-        } 
-    });
-});
-*/
+
 fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Food')
     .then(res => {
         if(!res.ok) {
