094c6ca7dfa1e7f38c17d59da8691803ab515816
zijadddd <zijad.doglod.20@size.ba>
2022-05-12 16:54:49 +0200

    git push --all origin

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 let counterForNewsMessage = 0;
-var root = false;
+let root = false;
 
 setInterval(() => {
-    let message = document.querySelector('#newsMessage');
+    let message = document.querySelector('#content__header__messages__message');
 
     const messages = [
         'Pomoći građanima Zenice da žive bolje omogućavajući da zdrava hrana bude jeftina i pristupačna.',
@@ -16,9 +16,8 @@ setInterval(() => {
 }, 3000);
 
 const deleteNewsMessage = () => {
-    let div = document.querySelector("#headerMessage");
+    let div = document.querySelector("#content__header__messages__message");
     div.style.display = "none";
-
 }
 
 fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Food')
@@ -29,19 +28,19 @@ fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Food')
         return res.json();
     })
     .then(data => {
-        const cardList = document.querySelector('#food');
+        const cardList = document.querySelector('#content__food__cards');
         let cards = '';
 
         data.forEach(element => {
             cards += `
-                <div class="card" style="width: 18rem; height: 22rem; margin: 0 20px; margin-top: 20px">
+                <div class="card" style="width: 18rem; height: auto; margin: 0 20px; margin-top: 20px" id="${element.id}">
                     <img src="${element.imageUrl}" class="card-img-top" alt="..." style="width: 100%; height: 60%;">
                     <div class="card-body">
                         <h5 class="card-title">${element.name}</h5>
                         <p class="card-text">Cijena: ${element.price} KM</p>
-                        <a href="#" class="btn" style="background-color: #F66B0E;"><span class="text-light">Naruči</span></a>
-                        <a href="#" class="btn rootButtons" style="background-color: #0AA1DD; display: none;"><span class="text-light">Uredi</span></a>
-                        <a href="#" class="btn rootButtons" style="background-color: #F32424; display: none;"><span class="text-light">Izbriši</span></a>
+                        <button href="" class="btn content__food__cards__root-buttons" style="background-color: #0AA1DD; margin: 1% 0; display: none;"><span class="text-light">Uredi</span></button>
+                        <button href="" class="btn content__food__cards__root-buttons" style="background-color: #F32424; margin: 1% 0; display: none;"><span class="text-light">Izbriši</span></button>
+                        <button href="" class="btn" style="background-color: #F66B0E; margin: 1% 0; width: 100%;" onclick="dodajUKorpu(this)"><span class="text-light">Naruči</span></button>
                     </div>
                 </div>                    
             `
@@ -51,19 +50,19 @@ fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Food')
     })
 
 const login = () => {
-    let temp = document.querySelector('#loginForm');
+    let temp = document.querySelector('#login');
     temp.style.display = 'block';
     temp.innerHTML = `
-        <form id="forma">
+        <form id="login__form">
             <div class="mb-3">
                 <label class="form-label">Username</label>
-                <input type="text" class="form-control" id="loginUsername" placeholder="Enter your username">
+                <input type="text" class="form-control" id="login__form__username" placeholder="Enter your username">
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="loginPassword" placeholder="Enter your password">
+                <input type="password" class="form-control" id="login__form__password" placeholder="Enter your password">
             </div>
-            <p id="errorMessage" class="text-danger"></p>
+            <p id="login__form__error-message" class="text-danger"></p>
             <div>
                 <input type="button" class="btn" style="background-color: #F66B0E; color: white;" onclick="loginForm()" value="Submit">
             </div>
@@ -74,7 +73,7 @@ const login = () => {
     temp.style.filter = 'blur(5px)';
 
     document.addEventListener('mouseup', (e) => {
-        temp = document.querySelector('#loginForm');
+        temp = document.querySelector('#login');
         if (!temp.contains(e.target)) {
             temp.style.display = 'none';
             temp = document.querySelector('#content');
@@ -84,12 +83,12 @@ const login = () => {
 }
 
 const loginForm = () => {
-    let loginUsername = document.querySelector('#loginUsername').value;
-    let loginPassword = document.querySelector('#loginPassword').value;
+    let loginUsername = document.querySelector('#login__form__username').value;
+    let loginPassword = document.querySelector('#login__form__password').value;
 
     if (loginUsername === 'root') {
         if (loginPassword === 'root') {
-            let temp = document.querySelector('#loginForm');
+            let temp = document.querySelector('#login');
             temp.style.display = 'none';
             temp = document.querySelector('#content');
             temp.style.filter = 'blur(0px)';
@@ -97,17 +96,17 @@ const loginForm = () => {
             popup();
             rootControls();
         } else {
-            document.querySelector('#errorMessage').innerText = 'Nepravilna lozinka.';
+            document.querySelector('#login__form__error-message').innerText = 'Nepravilna lozinka.';
         }
     } else {
-        document.querySelector('#errorMessage').innerText = 'Nepravilan username.';
+        document.querySelector('#login__form__error-message').innerText = 'Nepravilan username.';
     }
 }
 
 const rootControls = () => {
-    let cards = document.querySelectorAll('.rootButtons');
-    let loginButton = document.querySelector('#loginButton');
-    let logoutButton = document.querySelector('#logoutButton'); 
+    let cards = document.querySelectorAll('.content__food__cards__root-buttons');
+    let loginButton = document.querySelector('#content__navbar__navbar-collapse__login-button');
+    let logoutButton = document.querySelector('#content__navbar__navbar-collapse__logout-button'); 
 
     for (let i = 0; i < cards.length; i++) {
         cards[i].style.display = 'inline-block';
@@ -118,9 +117,9 @@ const rootControls = () => {
 }
 
 const logout = () => {
-    let cards = document.querySelectorAll('.rootButtons');
-    let loginButton = document.querySelector('#loginButton');
-    let logoutButton = document.querySelector('#logoutButton'); 
+    let cards = document.querySelectorAll('.content__food__cards__root-buttons');
+    let loginButton = document.querySelector('#content__navbar__navbar-collapse__login-button');
+    let logoutButton = document.querySelector('#content__navbar__navbar-collapse__logout-button'); 
 
     for (let i = 0; i < cards.length; i++) {
         cards[i].style.display = 'none';
@@ -133,11 +132,9 @@ const logout = () => {
 }
 
 const popup = () => {
-    let popup = document.querySelector('#loginoutPopUp');
+    let popup = document.querySelector('#pop-up');
     popup.style.display = 'flex';
-    popup.style.justifyContent = 'center';
-    popup.style.alignItems = 'center';
-    popup.style.textAlign = 'center';
+
     if (root) popup.innerText = 'Uspješno ste prijavljeni.';
     else popup.innerText = 'Uspješno ste odjavljeni.'
 
